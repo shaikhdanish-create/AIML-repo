2896b2dcc7f16203d15321b9b1caa35bda4a2b13
danish almer <shaikhkingdanish@gmail.com>
2026-09-18 15:15:29 +0530

    Add multiple column sorting

--- day1_sql_basics.sql
+++ day1_sql_basics.sql
@@ -93,3 +93,5 @@ SELECT first_name, last_name FROM employees WHERE first_name LIKE '_a%';
 SELECT first_name, last_name, salary FROM employees ORDER BY salary ASC;
 
 -- DESC sorts from high to low / Z to A
+
+-- Multiple-column sorting applies the second sort within ties of the first
