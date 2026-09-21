1e56542247a5880031022ee5ab918c66f008a110
danish almer <shaikhkingdanish@gmail.com>
2026-09-21 21:41:28 +0530

    Add JOIN AVG example

--- day3_joins.sql
+++ day3_joins.sql
@@ -98,3 +98,5 @@ SELECT c.customer_name, o.order_amount FROM customers c JOIN orders o ON c.custo
 SELECT c.customer_name, COUNT(o.order_id) AS order_count FROM customers c LEFT JOIN orders o ON c.customer_id = o.customer_id GROUP BY c.customer_name;
 
 SELECT c.customer_name, SUM(o.order_amount) AS total_spend FROM customers c JOIN orders o ON c.customer_id = o.customer_id GROUP BY c.customer_name;
+
+SELECT c.customer_name, AVG(o.order_amount) AS average_order FROM customers c JOIN orders o ON c.customer_id = o.customer_id GROUP BY c.customer_name;
