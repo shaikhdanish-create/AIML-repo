5a6217e1f2e0c07bcd1725126c7d557ee9a9fa74
danish almer <shaikhkingdanish@gmail.com>
2026-09-21 21:41:25 +0530

    Add JOIN SUM example

--- day3_joins.sql
+++ day3_joins.sql
@@ -96,3 +96,5 @@ SELECT c.customer_name AS customer, o.order_amount AS amount FROM customers c JO
 SELECT c.customer_name, o.order_amount FROM customers c JOIN orders o ON c.customer_id = o.customer_id WHERE o.order_amount > 1000;
 
 SELECT c.customer_name, COUNT(o.order_id) AS order_count FROM customers c LEFT JOIN orders o ON c.customer_id = o.customer_id GROUP BY c.customer_name;
+
+SELECT c.customer_name, SUM(o.order_amount) AS total_spend FROM customers c JOIN orders o ON c.customer_id = o.customer_id GROUP BY c.customer_name;
