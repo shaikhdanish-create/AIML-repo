c29c5835ed1ace43ac8ca34cbdddc4378e2c4d0e
danish almer <shaikhkingdanish@gmail.com>
2026-09-21 21:42:06 +0530

    Add top JOIN result

--- day3_joins.sql
+++ day3_joins.sql
@@ -113,3 +113,5 @@ SELECT c.customer_name, COUNT(o.order_id) AS order_count FROM customers c JOIN o
 SELECT c.customer_name, COALESCE(o.order_amount, 0) AS order_amount FROM customers c LEFT JOIN orders o ON c.customer_id = o.customer_id;
 
 SELECT c.customer_name, o.order_id FROM customers c JOIN orders o ON c.customer_id = o.customer_id WHERE c.city = 'Nagpur';
+
+SELECT c.customer_name, SUM(o.order_amount) AS total_spend FROM customers c JOIN orders o ON c.customer_id = o.customer_id GROUP BY c.customer_name ORDER BY total_spend DESC LIMIT 5;
